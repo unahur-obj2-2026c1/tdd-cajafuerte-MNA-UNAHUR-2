ec51155b43b98c9d46e1239ac6a82ef0d6565a2b
MNA-UNAHUR <marcelonahuel.andreassi@estudiantes.unahur.edu.ar>
2026-04-24 20:13:00 -0300

    feat: Editado el archivo 'CajaFuerteTest.java'

--- src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
+++ src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
@@ -1,11 +1,15 @@
 package ar.edu.unahur.obj2.tdd;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class CajaFuerteTest {
     
     @Test
-    public void test() {
+    public void queLaCajaFuerteEsteAbiertaAlCrearse() {
+        CajaFuerte caja = new CajaFuerte();
+        assertTrue(caja.estaAbierta());
         
     }
 
